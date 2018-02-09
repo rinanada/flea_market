4438ec40d35d159970886a0547fa659742cf2c89
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-02-09 22:10:51 +0900

    本固有の情報の表示可能に

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
+
   def new
     @book = Book.find(params[:book_id])
-    @order = Order.new
+    @tax = (@book.price + 200) * 0.08
+    @total_price = (@book.price + 200) * 1.08
   end
 
   def create
