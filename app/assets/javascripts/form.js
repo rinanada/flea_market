35cfe71f9d304fa36267adf31a46b0005249ada6
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-02-12 20:21:28 +0900

    購入者情報の編集時のjsアラートを追加

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,22 @@
 function process(){
-  var name = document.fm.name.value;
-  window.alert('hello \n world');
+  var result = '';
+  var user_detail = ['姓', '名','郵便番号','住所',];
+
+  for (var i = 0; i < 4 ; i++){
+    result
+    += user_detail[i]
+    + '：'
+    + document.fm.elements[i].value
+    + '\n';
+  }
+  result += '\nで変更します'
+  window.alert(result);
   return false;
 }
+
+
+// 姓：灘
+// 名：梨菜
+// 郵便番号：000-0000
+// 住所：とやま
+// 以上で変更します
