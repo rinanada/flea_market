ed0bda66e6bf3570caa63f75a817151ee7beee5a
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-02-18 22:57:16 +0900

    姓と名の間に半角を挟む

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
     if user_signed_in?
       @user_detail = UserDetail.find_by(user_id: current_user.id)
-      @mastname = @user_detail.try(:lastname) || ''
+      @mastname = @user_detail.try(:lastname) + ' ' || ''
       @mastname += @user_detail.try(:firstname) || '名無し'
       @mastname += 'さん'
     else
