004f4bcf42b4a536431212349de9b674c7ed5432
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-03-18 18:28:18 +0900

    booksテーブル、likesテーブルにカラム追加

--- db/migrate/20180318052304_create_likes.rb
+++ db/migrate/20180318052304_create_likes.rb
@@ -3,6 +3,8 @@ class CreateLikes < ActiveRecord::Migration[5.0]
     create_table :likes do |t|
 
       t.timestamps
+      t.references :user, index: true
+      t.references :book, index: true
     end
   end
 end
---
+++ db/migrate/20180318091050_add_like_count_to_books.rb
@@ -0,0 +1,5 @@
+class AddLikeCountToBooks < ActiveRecord::Migration[5.0]
+  def change
+    add_column :books, :likes_count, :integer
+  end
+end
