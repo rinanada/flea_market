655d904f5f0a8da52be9e92f4e9c10153dcefcfc
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-03-18 14:33:47 +0900

    Add assosiation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_one :exhibit
   has_one :order
+  has_many :likes, dependent: :destroy
 
   mount_uploader :content, BookUploader
   acts_as_taggable_on :categories
@@ -12,4 +13,8 @@ class Book < ApplicationRecord
   def total_price
     (price + 200) * 1.08.round
   end
+
+  # def likes_user
+  #   likes.user.
+  # end
 end
