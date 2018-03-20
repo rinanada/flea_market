a400c036a3a317cb8015df28436ebf28d4f56b23
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-03-20 20:34:19 +0900

    いいね機能の非同期機能追加。render templateを使用。

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :book, counter_cache: :likes_count
-  belongs_to :user
+  belongs_to :book, counter_cache: :likes_count, optional: true
+  belongs_to :user, optional: true
 end
