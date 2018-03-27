ca68fbaf4ecd9dda24097db96ce7bff21f75f6f5
Riyonann <riyonann@localhost.localdomain>
2018-03-27 17:51:19 +0900

    edit environments!

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,45 +3,57 @@ lock "3.10.1"
 
 set :application, "flea_market"
 set :repo_url, "git@github.com:rinanada/flea_market.git"
+set :default_stage, "production"
+set :scm, :git
+set :deploy_via, :remote_cache
 set :branch, "master"
 set :deploy_to, "/var/www/rails/flea_market"
+set :log_level, :debug
+set :pty, true
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets')
 set :keep_releases, 5
+set :unicorn_pid, "#[shared_path]/tmp/pids/unicorn.pid"
 
-
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  # アプリの再起動を行うタスク
   desc 'Restart application'
   task :restart do
-    invoke 'unicorn:restart'
-  end
-
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :mkdir, '-p', release_path.join('tmp')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
+  # linked_files で使用するファイルをアップロードするタスク
+  # deployが行われる前に実行する必要がある。
+  desc 'upload important files'
+  task :upload do
+    on roles(:app) do |host|
+      execute :mkdir, '-p', "#{shared_path}/config"
+      upload!('config/database.yml',"#{shared_path}/config/database.yml")
+      upload!('config/secrets.yml',"#{shared_path}/config/secrets.yml")
     end
   end
 
-  after  :publishing, :restart
-
+  # webサーバー再起動時にキャッシュを削除する
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+        execute :rm, '-rf', release_path.join('tmp/cache')
+      end
     end
+  end
+
+  # Flow の before, after のタイミングで上記タスクを実行
+  before :started, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+
+  # Unicorn 再起動タスク
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart' # lib/capustrano/tasks/unicorn.cap 内処理を実行
   end
 end
